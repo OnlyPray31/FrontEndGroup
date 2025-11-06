b1794e14392ea5f9f2217a30a1b2e8d3aedac1af
Onlyray31 <s22210670@student.unklab.ac.id>
2025-11-07 02:07:49 +0800

    file fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-const App = () => {
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer";
+
+export default function App() {
   return (
-    <div className="text-[25px] bg-blue-500 m-10 p-10 font-serif ">
-      Hello World
+    <div className="min-h-screen flex flex-col text-gray-800">
+      <Header />
+      <main className="flex-1 container  mx-auto px-4 py-6">
+        <Home />
+      </main>
+      <Footer />
     </div>
   );
-};
-
-export default App;
+}
